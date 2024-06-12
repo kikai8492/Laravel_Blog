b70c7d996a7d42755b3a41e6b5e65a8209360513
shuto kikai <yysskikai04@icloud.com>
2024-06-12 16:36:43 +0900

    commit

--- app/Http/Controllers/Blogscontroller.php
+++ app/Http/Controllers/Blogscontroller.php
@@ -8,26 +8,20 @@ use App\Http\Requests\BlogRequest;
 
 class BlogsController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    
     public function index()
     {
         $blogs = Blog::all();
         return view('blogs.index', ['blogs' => $blogs ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
+    
     public function create()
     {
         return view('blogs.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+    
     public function store(BlogRequest $request)
     {
         $validated = $request->validated();
@@ -36,27 +30,21 @@ class BlogsController extends Controller
 		return to_route('blogs.index') -> with('success','ブログを投稿しました');
     }
 
-    /**
-     * Display the specified resource.
-     */
+    
     public function show(string $id)
     {
         $blog = Blog::find($id);
         return view('blogs.show', compact('blog'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
+    
     public function edit(string $id)
     {
         $blog = Blog::find($id);
         return view('blogs.edit',['blog'=>$blog]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+    
     public function update(BlogRequest $request, string $id)
     {
         $blog = Blog::find($id);
@@ -65,9 +53,7 @@ class BlogsController extends Controller
         return to_route('blogs.index') -> with('success','ブログを更新しました');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+    
     public function destroy(string $id)
     {
         $blog = Blog::find($id);
