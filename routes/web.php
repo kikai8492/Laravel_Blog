75a30a34695c9226901eaf72e11d4a43a3707cd1
shuto kikai <yysskikai04@icloud.com>
2024-06-13 14:14:52 +0900

    commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\BlogsController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -25,3 +25,5 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::resource('/blogs', BlogsController::class);
